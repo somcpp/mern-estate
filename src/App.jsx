00a65875c685c750ce61055e9611babe2933d995
somcpp <saubhagya.sharma3.14@gmail.com>
2025-08-30 09:10:47 +0530

    complete sign in page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import About from './pages/About';
 import SignUp from './pages/SignUp';
